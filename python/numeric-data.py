f10a1b99213b59510f088244d1993e114d6a6069
ericdwatts <eric@ericdwatts.com>
2023-11-30 13:12:59 -0500

    numeric-data.py

---
+++ python/numeric-data.py
@@ -0,0 +1,35 @@
+# Print a description of the section
+print("Working with Numeric Data Types")
+
+# Assign an integer value to the variable myValue
+myValue = 1
+# Print the value of myValue
+print(myValue)
+# Print the data type of myValue
+print(type(myValue))
+# Print a message that includes the value and its data type
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Assign a floating-point value to myValue
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Assign a complex number value to myValue
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Assign a boolean value (True) to myValue
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Assign a boolean value (False) to myValue
+myValue = False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
